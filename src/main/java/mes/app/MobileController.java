2fe8ee1e19e80239106754753253738b2c5b963d
LeeJaeWook <ljw960214@naver.com>
2025-03-27 11:00:28 +0900

    250327_이재욱_모바일 공지사항 완료

--- src/main/java/mes/app/MobileController.java
+++ src/main/java/mes/app/MobileController.java
@@ -1,14 +1,27 @@
 package mes.app;
 
+import mes.app.mobile_notice.Service.NoticeBoardService;
+import mes.domain.DTO.FileResponseDto;
+import mes.domain.DTO.NoticeResponseDto;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 // 모바일 메뉴 컨트롤러
 @Controller
 @RequestMapping("/mobile")
 public class MobileController {
+
+    private final NoticeBoardService noticeBoardService;
+
+
+    public MobileController(NoticeBoardService noticeBoardService) {
+        this.noticeBoardService = noticeBoardService;
+    }
 
     @GetMapping("/ticket-list")
     public String ticketList(Model model) {
@@ -25,6 +38,25 @@ public class MobileController {
     public String currentStatusPage(Model model) {
         model.addAttribute("currentPage", "current-status");
         return "mobile/current-status"; // Return the Thymeleaf view name
+    }
+
+    @GetMapping("/notice/view")
+    public String NoticePage(Model modelMap, @RequestParam Integer id) {
+
+
+        NoticeResponseDto detail = noticeBoardService.NoticeView(id);
+
+        List<FileResponseDto> file = noticeBoardService.NoticeFile(id);
+
+        if(!file.isEmpty()){
+            modelMap.addAttribute("fileflag", true);
+        }else{
+            modelMap.addAttribute("fileflag", false);
+        }
+        modelMap.addAttribute("currentPage", "notice");
+        modelMap.addAttribute("notice", detail);
+
+        return "mobile/noticeView";
     }
 
     @GetMapping("/fsr-register")
---
+++ src/main/java/mes/app/mobile_notice/Service/FileUtilService.java
@@ -0,0 +1,37 @@
+package mes.app.mobile_notice.Service;
+
+import mes.app.mobile_notice.Service.FileDownloadHandler;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class FileUtilService {
+
+    public FileDownloadHandler getFileDownloadHandler(List<Map<String, Object>> files){
+        if(files.size() == 1) return new SingleFileDownloadHandler();
+        return new ZipFileDownloadHandler();
+
+    }
+
+    public String generateFileName(List<Map<String, Object>> result, String displayName) {
+        if(result.size() == 1) return result.get(0).get("fileornm").toString();
+        String today = LocalDate.now().format(DateTimeFormatter.ofPattern("yyyyMMdd"));
+        return today + displayName;
+    }
+
+    public Object prepareRequestData(List<Map<String, Object>> files){
+        if(files.size() == 1) return files.get(0).get("filesvnm").toString();
+        return files.stream().map(file -> {
+            Map<String, String> fileMap = new HashMap<>();
+            fileMap.put("filesvnm", file.get("filesvnm").toString());
+            fileMap.put("fileornm", file.get("fileornm").toString());
+            return fileMap;
+        }).collect(Collectors.toList());
+    }
+}
